2a336f48d06aa7e0ac98e393dbb8e239bb7ca10b
MirageM <imiragesz@gmail.com>
2024-07-19 23:22:10 -0400

    distances.sort(key = lambda x: -x[0]);

---
+++ kClosest75.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def kClosest(self, points, k):
+        """
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        distances = []
+        result = []
+        for x, y in points:
+            distance = sqrt(x**2 + y**2)
+            distances.append([distance, x, y])
+        distances.sort(key = lambda x: -x[0]);
+        while k > 0:
+            distance, x, y = distances.pop()
+            result.append([x, y])
+            k -= 1
+        return result
